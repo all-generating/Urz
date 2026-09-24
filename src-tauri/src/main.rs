477290b8721bcf14f94317606f38bac26b4d3405
all-generating <all-generating@github.com>
2026-09-24 22:27:18 +0300

    A little refactor + github actions

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,20 +1,5 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-mod commands;
-mod models;
-mod password;
-
-use commands::{copy_to_clipboard, generate_password_cmd, ClipboardState};
-
 fn main() {
-    tauri::Builder::default()
-        .plugin(tauri_plugin_shell::init())
-        .plugin(tauri_plugin_clipboard_manager::init())
-        .manage(ClipboardState::new())
-        .invoke_handler(tauri::generate_handler![
-            generate_password_cmd,
-            copy_to_clipboard
-        ])
-        .run(tauri::generate_context!())
-        .expect("error while running tauri application");
+    urz::run();
 }
